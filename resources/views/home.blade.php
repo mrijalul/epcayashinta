97835ed5737ae21a9fcb249e2e76a4a3a3433c05
rijalul <muhammadrijalulmutaqin@gmail.com>
2020-11-21 00:53:03 +0700

    edit color header main menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-	<h1 class="display-4">Main Menu</h1>
+	<h1 class="display-4" style="color: #fff">Main Menu</h1>
 </div>
 <div class="row">
     <div class="col-md-8 blog-main">
