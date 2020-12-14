ca8dcff5a958005a591e0f4d38785fef0107b523
rijalul <muhammadrijalulmutaqin@gmail.com>
2020-12-14 15:46:31 +0700

    change url home navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,7 @@
 	<div id="app" style="background: #7579e7; height: 100%;">
 		<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
 			<div class="container">
-				<a class="navbar-brand kathen" href="{{ url('/') }}">
+				<a class="navbar-brand kathen" href="{{ route('home') }}">
 					{{ config('app.name', 'Laravel') }}
 				</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
